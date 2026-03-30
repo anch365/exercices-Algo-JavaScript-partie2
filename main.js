f14c85affd514abd4c51cc8c024c8615a3f966e1
anch365 <mchindaanchoura@gmail.com>
2026-03-30 13:26:42 +0200

    Définir 3 variables

--- main.js
+++ main.js
@@ -9,3 +9,15 @@
 // EXERCICE 1: afficher une boite de dialogue
 let sms = "Time's money";
 alert(sms);
+
+// EXERCIE 2: Définir 3 variables 
+let lastName = "Abdou";
+
+let firstName = "Fatima";
+
+let city = "Montbéliard";
+alert(`
+  Nom: ${lastName} \n 
+  Prenom: ${firstName} \n 
+  Ville: ${city} 
+    `);
