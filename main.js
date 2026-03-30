e628f6912cfbd090b52cdef0b830bbdeeb04460b
anch365 <mchindaanchoura@gmail.com>
2026-03-30 12:56:57 +0200

    Afficher une boîte de dialogue

---
+++ main.js
@@ -0,0 +1,11 @@
+// alert("Hello world!");
+// let message = "vive la vie";
+// alert(35);
+
+// let message = prompt("How are you ?");
+// // alert(message);
+// console.log(message);
+
+// EXERCICE 1: afficher une boite de dialogue
+let sms = "Time's money";
+alert(sms);
